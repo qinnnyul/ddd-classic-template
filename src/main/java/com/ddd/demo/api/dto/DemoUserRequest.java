9790dd932c38e7d67c3709f8bc0eb9dd695290ea
Yulin <yulin.qin.ext@gmail.com>
2019-09-18 11:42:20 +0800

    [Yulin] 增加资源库

--- src/main/java/com/ddd/demo/api/dto/DemoUserRequest.java
+++ src/main/java/com/ddd/demo/api/dto/DemoUserRequest.java
@@ -1,4 +1,13 @@
 package com.ddd.demo.api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DemoUserRequest {
+
+    private String name;
+
+    private int age;
 }
