0447964f038e35e8e6607ff631e0574b6a4739b9
Yulin <yulin.qin.ext@gmail.com>
2019-09-19 22:44:35 +0800

    [Yulin] 命名规范

--- src/test/java/com/ddd/demo/NamingConventionTest.java
+++ src/test/java/com/ddd/demo/NamingConventionTest.java
@@ -10,25 +10,41 @@ import org.junit.runner.RunWith;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @RunWith(ArchUnitRunner.class)
-@AnalyzeClasses(packages = {"com.nbcb.billservice.bill", "com.nbcb.billservice.command"},
+@AnalyzeClasses(packages = {"com.ddd.demo"},
         importOptions = {ImportOption.DoNotIncludeTests.class, ImportOption.DoNotIncludeJars.class})
 public class NamingConventionTest {
 
     @ArchTest
-    public static final ArchRule Controller_must_endwith_api_in_application_layer = classes()
+    public static final ArchRule Controller_must_endwith_controller_in_application_layer = classes()
             .that()
-            .resideInAnyPackage("com.nbcb.billservice.command.application.api", "com.nbcb.billservice.bill.application.api")
+            .resideInAnyPackage("com.ddd.demo.api")
             .should()
-            .haveNameMatching(".*Api")
-            .as("Restful API的类应该以Api结尾");
+            .haveNameMatching(".*Controller")
+            .as("Restful API的类应该以Controller结尾");
+
+    @ArchTest
+    public static final ArchRule Dto_must_endwith_request_in_application_layer = classes()
+            .that()
+            .resideInAnyPackage("com.ddd.demo.api.dto").should().haveNameMatching(".*Request.*")
+            .orShould().haveNameMatching(".*Response.*")
+            .as("DTO的类应该以Request or Response结尾");
 
 
     @ArchTest
     public static final ArchRule ApplicationService_must_endwith_AppService_in_application_layer = classes()
             .that()
-            .resideInAnyPackage("com.nbcb.billservice.command.application.service", "com.nbcb.billservice.bill.application.service")
+            .resideInAnyPackage("com.ddd.demo.application")
             .should()
             .haveNameMatching(".*AppService")
             .as("应用服务的类应该以AppService结尾");
+
+    @ArchTest
+    public static final ArchRule Dao_must_endwith_dao_in_infrastructure_layer = classes()
+            .that()
+            .resideInAnyPackage("com.ddd.demo.infrastructure.dao")
+            .should()
+            .haveNameMatching(".*Dao")
+            .as("基础设施层的DAO类应该以Dao结尾");
+
 
 }
