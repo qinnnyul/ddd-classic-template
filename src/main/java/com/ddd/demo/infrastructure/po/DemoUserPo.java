0411190e4a590a3b589dcd101d576596249b4775
Yulin <yulin.qin.ext@gmail.com>
2019-11-12 20:34:04 +0800

    [Yulin] use in-memory db for demo

--- src/main/java/com/ddd/demo/infrastructure/po/DemoUserPo.java
+++ src/main/java/com/ddd/demo/infrastructure/po/DemoUserPo.java
@@ -5,10 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Setter
 @Getter
 @Builder
+@Entity
 public class DemoUserPo {
+    @Id
     private String id;
 
     private String name;
